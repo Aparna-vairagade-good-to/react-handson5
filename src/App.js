573445e4c9b3b9d9f4ff41c4bce7d829887f4acd
Aparna-vairagade-good-to <vairagadeaparna123456>
2023-04-10 19:34:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import CounterClick from './Components/CounterClick'
+import CounterMouseover from './Components/CounterMouseover'
+import HocComponent from './Components/HocComponent'
+import "./App.css"
+
+const App = () => {
+  return (
+    <div>
+    <h3>HOC Component Example</h3>
+      <CounterClick/>
+      <CounterMouseover/>
+      <HocComponent/>
+    </div>
+  )
+}
+
+export default App;
